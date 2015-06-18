d7cc91a9f8876a3e73dfeada1b8fc0c26e3dd8d3
enricobacis <enrico.bacis@gmail.com>
2015-06-18 11:51:02 +0200

    Improve rate.py with a variant that can be used with with statement.

--- virustotal/rate.py
+++ virustotal/rate.py
@@ -1,15 +1,20 @@
+from contextlib import contextmanager
 from threading import Semaphore, Timer
 
 def ratelimit(limit, every):
-	def limitdecorator(fn):
-		semaphore = Semaphore(limit)
-		def _timeout():
-			semaphore.release()
-		def _fn(*args, **kwargs):
-			semaphore.acquire()
-			result = fn(*args, **kwargs)
-			Timer(every, _timeout).start()
-			return result
-		return _fn
-	return limitdecorator
+    def limitdecorator(fn):
+        semaphore = Semaphore(limit)
+        def _fn(*args, **kwargs):
+            semaphore.acquire()
+            result = fn(*args, **kwargs)
+            Timer(every, semaphore.release).start()
+            return result
+        return _fn
+    return limitdecorator
+
+@contextmanager
+def ratelimiter(semaphore, releaseafter):
+    semaphore.acquire()
+    yield
+    Timer(releaseafter, semaphore.release).start()
 
