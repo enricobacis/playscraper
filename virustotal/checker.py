4938bff00fca5bfcc8b7f9db83bbe02a387133fd
enricobacis <enrico.bacis@gmail.com>
2015-06-19 19:08:25 +0200

    Refactor virustotal/checker.py and add addtodownload function to add all apks in the basedir to the download table.

--- virustotal/checker.py
+++ virustotal/checker.py
@@ -2,11 +2,12 @@
 
 VIRUSTOTAL_APIKEY = None
 
-from contextlib import closing
+from contextlib import closing, contextmanager
 from argparse import ArgumentParser
 from colorama import init, Fore
 from config import *
 from time import time
+from glob import glob
 from vt import VirusTotal
 import humanfriendly as hf
 import os.path
@@ -15,7 +16,8 @@ import signal
 
 init(autoreset=True)
 
-create = ('CREATE TABLE IF NOT EXISTS virus (pkg PRIMARY KEY, id, uploaded, detected)')
+create1 = 'CREATE TABLE IF NOT EXISTS virus (pkg PRIMARY KEY, id, uploaded, detected)'
+create2 = 'CREATE TABLE IF NOT EXISTS download (pkg PRIMARY KEY, path TEXT)'
 select1 = 'SELECT pkg, path FROM download WHERE pkg NOT IN (SELECT pkg FROM virus) LIMIT 1'
 select2 = 'SELECT pkg, id FROM virus WHERE detected = -1 AND uploaded < ? LIMIT 1'
 insert = 'INSERT INTO virus VALUES (?, ?, ?, ?)'
@@ -27,16 +29,27 @@ def exit():
 
 class Checker():
 
-    def __init__(self, basepath):
+    def __init__(self, dbname, basedir):
         self.api = VirusTotal(VIRUSTOTAL_APIKEY)
-        self.basepath = basepath
+        self.basedir = basedir
+        self.dbname = dbname
+        with self.getdbcursor() as (db, cursor):
+            cursor.execute(create1)
+            cursor.execute(create2)
 
-    def scan(self, db):
-        with closing(db.cursor()) as cursor:
+    @contextmanager
+    def getdbcursor(self):
+        with sqlite3.connect(self.dbname, timeout=10) as db:
+            with closing(db.cursor()) as cursor:
+                yield db, cursor
+            db.commit()
+
+    def scan(self):
+        with self.getdbcursor() as (db, cursor):
             selected = cursor.execute(select1).fetchall()
             for pkg, path in selected:
                 print Fore.BLUE + 'Uploading %s for scan ...' % pkg,
-                path = os.path.abspath(os.path.join(self.basepath, path))
+                path = os.path.abspath(os.path.join(self.basedir, path))
                 if os.path.getsize(path) >= hf.parse_size('32M'):
                     cursor.execute(insert, (pkg, "", "", -2))
                     print Fore.RED + 'File too big for VirusTotal'
@@ -51,8 +64,8 @@ class Checker():
                 db.commit()
         return len(selected)
 
-    def result(self, db):
-        with closing(db.cursor()) as cursor:
+    def result(self):
+        with self.getdbcursor() as (db, cursor):
             selected = cursor.execute(select2, [time() - 4800]).fetchall()
             for pkg, id in selected:
                 try:
@@ -67,27 +80,27 @@ class Checker():
 
         return len(selected)
 
-    def createtable(self, db):
-        with closing(db.cursor()) as cursor:
-            cursor.execute(create)
-            db.commit()
-
-    def checkall(self, dbname):
-        with sqlite3.connect(dbname, timeout=10) as db:
-            self.createtable(db)
-            while True:
-                scanned = self.scan(db)
-                resulted = self.result(db)
-                if not scanned and not resulted:
-                    print 'Everything done for now'
-                    break
+    def checkall(self):
+        while any((self.scan(), self.result())): pass
+        print 'Everything done for now'
         exit()
 
+    def addtodownload(self):
+        with self.getdbcursor() as (db, cursor):
+            for apk in glob(os.path.join(self.basedir, '*.apk')):
+                apk = os.path.basename(apk)
+                pkg = os.path.splitext(apk)[0]
+                cursor.execute('INSERT INTO download VALUES (?, ?)', (pkg, apk))
+            db.commit()
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit)
     parser = ArgumentParser(description='Check apks with VirusTotal')
     parser.add_argument('DB', help='sqlite3 apps db')
     parser.add_argument('-b', '--basedir', default='.', help='base apk dir')
+    parser.add_argument('-a', '--add', action='store_true', help='add apks')
     args = parser.parse_args()
-    signal.signal(signal.SIGINT, exit)
-    Checker(args.basedir).checkall(args.DB)
+    checker = Checker(args.DB, args.basedir)
+    if args.add: checker.addtodownload()
+    checker.checkall()
 
