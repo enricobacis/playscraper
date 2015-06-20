e7c7956370b0c38b16b49b49cdd3bab2582573cc
enricobacis <enrico.bacis@gmail.com>
2015-06-20 14:10:42 +0200

    Add docstrings to rate.py

--- virustotal/rate.py
+++ virustotal/rate.py
@@ -3,6 +3,7 @@ from threading import Timer, Semaphore
 from functools import wraps
 
 def ratelimit(limit, every):
+    """Decorator to limit invocations. *limit* calls per *every* seconds."""
     def limitdecorator(fn):
         semaphore = Semaphore(limit)
         @wraps(fn)
@@ -16,6 +17,8 @@ def ratelimit(limit, every):
 
 @contextmanager
 def ratelimiter(semaphore, releaseafter):
+    """Acquires the semaphore, executes user part and starts the timer for
+       releasing the semaphore. Use it with with statement."""
     semaphore.acquire()
     yield
     Timer(releaseafter, semaphore.release).start()
