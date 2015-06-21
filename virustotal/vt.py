e400d5dd65bee6c8db8b5595fb56a8adaeb425f5
enricobacis <enrico.bacis@gmail.com>
2015-06-21 19:14:36 +0200

    Add docstring to virustotal API

--- virustotal/vt.py
+++ virustotal/vt.py
@@ -5,13 +5,16 @@ import requests
 
 
 class VirusTotal():
+    """This class is used to communicate to VirusTotal through its APIs."""
 
     def __init__(self, apikey, limit=4, every=61):
+        """Initialize the VirusTotal API limiting the reqs per timeframe."""
         self.semaphore = Semaphore(limit)
         self.apikey = apikey
         self.every = every
 
     def scan(self, path):
+        """Upload a file for scanning. It returns the resource ID."""
         with ratelimiter(self.semaphore, self.every):
             with open(path, 'rb') as f:
                 params = {'apikey': self.apikey}
@@ -21,16 +24,19 @@ class VirusTotal():
             return res.json()['resource']
 
     def get_report(self, resource):
+        """Get the report for a specific resource."""
         with ratelimiter(self.semaphore, self.every):
             params = {'apikey': self.apikey, 'resource': resource}
             url = 'https://www.virustotal.com/vtapi/v2/file/report'
             return requests.get(url, params=params).json()
 
     def get_num_detected(self, resource):
+        """Get the number of engines that reported a threat."""
         report = self.get_report(resource)
         return sum(scan['detected'] for av, scan in report['scans'].items())
 
     def get_percent_detected(self, resource):
+        """Get the percentage of engines that reported a threat."""
         report = self.get_report(resource)
         return (sum(scan['detected'] for av, scan in report['scans'].items()) /
                 float(len(report['scans'])))
