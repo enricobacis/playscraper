4468827795606ae57c5a7d62f5b2f08d93387f39
enricobacis <enrico.bacis@gmail.com>
2015-06-22 17:04:46 +0200

    Use refresh only if required in parameters (?refresh=something)

--- virustotal/server.py
+++ virustotal/server.py
@@ -2,14 +2,14 @@
 
 from contextlib import closing
 from sqlite3 import connect
-from bottle import template, route, run
+from bottle import request, template, route, run
 
 @route('/virustotal/<db>')
 def virus(db):
     with connect(db, timeout=10) as connection:
         with closing(connection.cursor()) as cursor:
             cursor.execute('SELECT detected, count(*) FROM virus GROUP BY detected ORDER BY detected')
-            return template('virustotal', title=db, cursor=cursor, refresh=10)
+            return template('virustotal', title=db, cursor=cursor, refresh=request.query.refresh)
 
 run(host='0.0.0.0')
 
