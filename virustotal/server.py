1a6b7eec67b6a2bd1b0ffc315c48a03ca9a56032
enricobacis <enrico.bacis@gmail.com>
2015-06-23 10:17:58 +0200

    virustotal/server.py does not create the database if it does not exist.

--- virustotal/server.py
+++ virustotal/server.py
@@ -2,10 +2,13 @@
 
 from contextlib import closing
 from sqlite3 import connect
+from os.path import isfile
 from bottle import request, template, route, run
 
 @route('/virustotal/<db>')
 def virus(db):
+    if not isfile(db):
+        return 'The database does not exist: "%s"' % db
     with connect(db, timeout=10) as connection:
         with closing(connection.cursor()) as cursor:
             cursor.execute('SELECT detected, count(*) FROM virus GROUP BY detected ORDER BY detected')
